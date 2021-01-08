1d7a1ae365cac54243831550699f7f181ca73ec6
Brody Nusbickel <brody.nusbickel@gmail.com>
2021-01-08 01:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let\'s talk about space";
+var topic = "Let\'s talk about space!";
 
-var conversation = `${greetSpecialGuest} ${topic}.`
+var conversation = `${greetSpecialGuest} ${topic}`
 ;
